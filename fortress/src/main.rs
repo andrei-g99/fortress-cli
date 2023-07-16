889372804ba8b62cfb5276e8a09fc2d597382405
andrei-g99 <bitmap901@gmail.com>
2023-07-16 21:48:58 +0200

    command parameter input

--- fortress/src/main.rs
+++ fortress/src/main.rs
@@ -1,14 +1,42 @@
 mod fortress_lib;
+use pbkdf2::{pbkdf2, hmac};
+use hmac::Hmac;
+use sha2::Sha256;
+use std::env;
 
 fn main() {
+    // Get command-line arguments
+    let args: Vec<String> = env::args().collect();
 
-    let key : [u8; 32] = [1, 2, 3, 4, 5, 6, 7, 8,
-    9, 10, 11, 12, 13, 14, 15, 16,
-    17, 18, 19, 20, 21, 22, 23, 24,
-    25, 26, 27, 28, 29, 30, 31, 32];
+    if args.len() < 3 {
+        println!("Please provide the required input.");
+        return;
+    }
 
-    //fortress_lib::encrypt("plaintext.txt", "ciphertext.bin", key);
-    let padding_byte : u8 = fortress_lib::decrypt("plaintext_decrypted.txt", "ciphertext.bin", key);
-    fortress_lib::depad("plaintext_decrypted.txt", padding_byte);
+    if args[1] == "encrypt"
+    {
+        let mut buf = [0u8; 32];
+        pbkdf2::<Hmac<Sha256>>(args[4].as_bytes(), b"salt", 600_000, &mut buf)
+        .expect("HMAC can be initialized with any key length");
+
+        fortress_lib::encrypt(args[2].as_str(), args[3].as_str(), buf);
+
+
+    }
+    else if args[1] == "decrypt"
+    {
+
+        let mut buf = [0u8; 32];
+        pbkdf2::<Hmac<Sha256>>(args[4].as_bytes(), b"salt", 600_000, &mut buf)
+        .expect("HMAC can be initialized with any key length");
+
+        let padding_byte : u8 = fortress_lib::decrypt(args[2].as_str(), args[3].as_str(), buf);
+        fortress_lib::depad(args[2].as_str(), padding_byte);
+
+    }
+    else
+    {
+        panic!("First argument must be either 'encrypt' or 'decrypt' e.g.\n cargo run encrypt plaintext.txt ciphertext.bin passphrase123");
+    }
 
 }
